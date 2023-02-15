5f8514508263ae3b6d7f349bfb8cf62c5613168d
Rovnin Nikita <Nikita.Rovnin@gmail.com>
2023-02-15 23:37:10 +0700

    Fix

--- task1/task.js
+++ task1/task.js
@@ -1,9 +1,10 @@
 const timerStart = document.getElementById('timer')
 
-setInterval(() => {
+let timer = setInterval(() => {
     if (timerStart.textContent == 0) {
         alert('You won in competition!')
+        clearTimeout(timer)
     } else {
         timerStart.textContent = Number(timerStart.textContent) - 1
     }
-},1000)
+},1)
--- task2/task.js
+++ task2/task.js
@@ -1,17 +1,6 @@
 const quantityClick = document.getElementById('clicker__counter')
 const img = document.getElementById('cookie')
-let switcher = false
-
 
 img.onclick = () => {
-    quantityClick.textContent = Number(quantityClick.textContent) + 1
-    if (switcher) {
-        img.width -= 50
-        img.height -= 50
-        switcher = false
-    } else {
-        img.width += 50
-        img.height += 50
-        switcher = true
-    }
+    img.width = ++quantityClick.textContent % 2 ? 250 : 200
 }
